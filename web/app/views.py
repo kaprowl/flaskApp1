2392733b3a316f06288dae5256bded248b7f8f03
kaprowl <aucarapon.m1@gmail.com>
2023-02-19 22:07:42 +0700

    lab11_v1

--- web/app/views.py
+++ web/app/views.py
@@ -1,11 +1,11 @@
 from flask import (jsonify, render_template,
-                   request, url_for, flash, redirect)
+                   request, url_for, flash, redirect, Flask)
 import json
 from sqlalchemy.sql import text
 from app import app
 from app import db
 from app.models.contact import Contact
-
+from app.models.BlogEntry import BlogEntry
 
 @app.route('/')
 def home():
@@ -56,6 +56,8 @@ def lab10_phonebook():
         return lab10_db_contacts()
     return app.send_static_file('lab10_phonebook.html')
 
+
+
 @app.route("/lab10/contacts")
 def lab10_db_contacts():
     contacts = []
@@ -65,6 +67,8 @@ def lab10_db_contacts():
     app.logger.debug("DB Contacts: " + str(contacts))
 
     return jsonify(contacts)
+
+
 
 @app.route('/lab10/remove_contact', methods=('GET', 'POST'))
 def lab10_remove_contacts():
@@ -80,3 +84,73 @@ def lab10_remove_contacts():
             app.logger.debug(ex)
             raise
     return lab10_db_contacts()
+
+# @app.route('/lab11', methods=('GET', 'POST'))
+# def lab11():
+#     return app.send_static_file('lab11_microblog.html')
+
+@app.route("/lab11/BlogEntry")
+def lab11_db_BlogEntry():
+    contacts = []
+    db_contacts = BlogEntry.query.all()
+
+    contacts = list(map(lambda x: x.to_dict(), db_contacts))
+    app.logger.debug("DB Contacts: " + str(contacts))
+
+    return jsonify(contacts)
+
+@app.route('/lab11/remove_BlogEntry', methods=('GET', 'POST'))
+def lab11_remove_BlogEntry():
+    app.logger.debug("LAB11 - REMOVE")
+    if request.method == 'POST':
+        result = request.form.to_dict()
+        id_ = result.get('id', '')
+        try:
+            contact = BlogEntry.query.get(id_)
+            db.session.delete(contact)
+            db.session.commit()
+        except Exception as ex:
+            app.logger.debug(ex)
+            raise
+    return lab11_db_BlogEntry()
+    
+@app.route('/lab11', methods=('GET', 'POST'))
+def lab11():
+    if request.method == 'POST':
+        result = request.form.to_dict()
+        app.logger.debug(str(result))
+        id_ = result.get('id', '')
+        validated = True
+        validated_dict = dict()
+        valid_keys = ['name', 'message', 'email']
+
+        # validate the input
+        for key in result:
+            app.logger.debug(key, result[key])
+            # screen of unrelated inputs
+            if key not in valid_keys:
+                continue
+
+            value = result[key].strip()
+            if not value or value == 'undefined':
+                validated = False
+                break
+            validated_dict[key] = value
+
+        if validated:
+            app.logger.debug('validated dict: ' + str(validated_dict))
+            # if there is no id: create a new blog entry
+            if not id_:
+                entry = BlogEntry(**validated_dict)
+                app.logger.debug(str(entry))
+                db.session.add(entry)
+            # if there is an id already: update the blog entry
+            else:
+                contact = BlogEntry.query.get(id_)
+                contact.update(**validated_dict)
+
+            db.session.commit()
+
+        return lab11_db_BlogEntry()
+    return app.send_static_file('lab11_microblog.html')
+
--- web/manage.py
+++ web/manage.py
@@ -2,7 +2,7 @@ from flask.cli import FlaskGroup
 
 from app import app, db
 from app.models.contact import Contact
-
+from app.models.BlogEntry import BlogEntry
 cli = FlaskGroup(app)
 
 @cli.command("create_db")
@@ -15,6 +15,9 @@ def create_db():
 def seed_db():
     db.session.add(
         Contact(firstname='สมชาย', lastname='ทรงแบด', phone='081-111-1111'))
+    db.session.add(
+        BlogEntry(name='everves', message='finesrdvn', email='pesrvpirewm@ghpetrjgh.com')
+    )
     db.session.commit()
 
 if __name__ == "__main__":
