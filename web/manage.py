4b225cc982b5b48b67a26dd6402e8ba25e495ddc
kaprowl <aucarapon.m1@gmail.com>
2023-02-24 16:25:55 +0700

    lab12

--- web/manage.py
+++ web/manage.py
@@ -1,7 +1,8 @@
 from flask.cli import FlaskGroup
-
+from werkzeug.security import generate_password_hash
 from app import app, db
 from app.models.contact import Contact
+from app.models.authuser import AuthUser, PrivateContact
 from app.models.BlogEntry import BlogEntry
 cli = FlaskGroup(app)
 
@@ -13,11 +14,18 @@ def create_db():
 
 @cli.command("seed_db")
 def seed_db():
+    db.session.add(AuthUser(email="flask@204212", name='สมชาย ทรงแบด',
+                            password=generate_password_hash('1234',
+                                                            method='sha256'),
+                            avatar_url='https://ui-avatars.com/api/?name=\สมชาย+ทรงแบด&background=83ee03&color=fff'))
     db.session.add(
         Contact(firstname='สมชาย', lastname='ทรงแบด', phone='081-111-1111'))
     db.session.add(
         BlogEntry(name='everves', message='finesrdvn', email='pesrvpirewm@ghpetrjgh.com')
     )
+    db.session.add(
+       PrivateContact(firstname='ส้มโอ', lastname='โอเค',
+                      phone='081-111-1112', owner_id=1))
     db.session.commit()
 
 if __name__ == "__main__":
